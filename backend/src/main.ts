b7e56d8761e5a08c2f725452006cd71f31e25a44
andreglock <andreglock@gmail.com>
2025-03-30 10:30:53 +0200

    Add global package.json and format.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,12 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['debug','error', 'warn'],
+    logger: ['debug', 'error', 'warn'],
   });
   app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
-  
+
   Logger.debug(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
